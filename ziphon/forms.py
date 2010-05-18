076b23d541128d8ab8c13099294ee23747e3ff7e
Oladayo Osikoya <alwaysdeone@gmail.com>
2010-05-18 16:46:34 +0100

    Used modelforms to avoid redundancy.

--- ziphon/forms.py
+++ ziphon/forms.py
@@ -1,19 +1,6 @@
 from django import forms
+from thedebaters.ziphon.models import Person
 
-class PersonForm(forms.Form):
-    first_name = forms.CharField(max_length=20)
-    last_name = forms.CharField(max_length=20)
-    home_phone = forms.CharField(max_length=20)
-    office_phone = forms.CharField(max_length=20)
-    mobile_phone = forms. CharField(max_length=20)
-    email = forms.EmailField()
-    address = forms.CharField(widget=forms.Textarea)
-
-    date_of_birth = forms.DateField()
-    state_of_origin = forms.CharField(max_length=20)
-    marital_status = forms.CharField(max_length=20)
-    occupation = forms.CharField(max_length=50)
-    hobbies = forms.CharField(widget=forms.Textarea)
-
-    passport_photo = forms.ImageField()
-    full_photo = forms.ImageField()
+class PersonForm(forms.ModelForm):
+    class Meta:
+	model = Person
--- ziphon/models.py
+++ ziphon/models.py
@@ -1,8 +1,62 @@
 from django.db import models
 
+SEX_CHOICES = (
+    ("Male", "Male"),
+    ("Female", "Female"),
+)
+
+STATE_CHOICES = (
+    ("Abia", "Abia"),
+    ("Abuja", "Abuja"),
+    ("Adamawa", "Adamawa"),
+    ("Akwa Ibom", "Akwa Ibom"),
+    ("Anambra", "Anambra"),
+    ("Bauchi", "Bauchi"),
+    ("Bayelsa", "Bayelsa"),
+    ("Benue", "Benue"),
+    ("Borno", "Borno"),
+    ("Cross-River", "Cross-River"),
+    ("Delta", "Delta"),
+    ("Ebonyi", "Ebonyi"),
+    ("Edo", "Edo"),
+    ("Ekiti", "Ekiti"),
+    ("Enugu", "Enugu"),
+    ("Gombe", "Gombe"),
+    ("Imo", "Imo"),
+    ("Jigawa", "Jigawa"),
+    ("Kaduna", "Kaduna"),
+    ("Kano", "Kano"),
+    ("Katsina", "Katsina"),
+    ("Kebbi", "Kebbi"),
+    ("Kogi", "Kogi"),
+    ("Nasarawa", "Nasarawa"),
+    ("Niger", "Niger"),
+    ("Ogun", "Ogun"),
+    ("Ondo", "Ondo"),
+    ("Osun", "Osun"),
+    ("Oyo", "Oyo"),
+    ("Kwara", "Kwara"),
+    ("Lagos", "Lagos"),
+    ("Plateau", "Plateau"),
+    ("Rivers", "Rivers"),
+    ("Sokoto", "Sokoto"),
+    ("Taraba", "Taraba"),
+    ("Yobe", "Yobe"),
+    ("Zamfara", "Zamfara")
+)
+
+RELATIONSHIP_STATUS_CHOICES = (
+    ("Single", "Single"),
+    ("In a committed relationship", "In a committed relationship"),
+    ("Married", "Married"),
+    ("Separated", "Separated"),
+    ("Divorced", "Divorced"),
+)
+
 class Person(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
+    sex = models.CharField(max_length=6, choices=SEX_CHOICES)
     home_phone = models.CharField(max_length=20)
     office_phone = models.CharField(max_length=20)
     mobile_phone = models.CharField(max_length=20)
@@ -10,16 +64,10 @@ class Person(models.Model):
     address = models.TextField()
 
     date_of_birth = models.DateField()
-    state_of_origin = models.CharField(max_length=20)
-    marital_status = models.CharField(max_length=20)
+    state_of_origin = models.CharField(max_length=20, choices=STATE_CHOICES)
+    relationship_status = models.CharField(max_length=20, choices=RELATIONSHIP_STATUS_CHOICES)
     occupation = models.CharField(max_length=50)
     hobbies = models.TextField()
 
-    passport_photo = models.ImageField(upload_to="photo/passport")
-    full_photo = models.ImageField(upload_to="photo/full")
-
     def __unicode__(self):
 	return "%s, %s" % (self.first_name, self.last_name)
-    
-class NextOfKin(models.Model):
-    pass
